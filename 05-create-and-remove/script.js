6c651e3521af4cf07e8b125661ccfaf755d90fb7
sjsiwat <sj.siwat@gmail.com>
2026-08-10 15:13:54 +0700

    done ex5

--- 05-create-and-remove/script.js
+++ 05-create-and-remove/script.js
@@ -3,12 +3,17 @@
 
 // TODO 1: Select #item-input, #add-item-btn, #items (the <ul>), and
 // #item-count (the <span>).
-
+const itemInput = document.getElementById("item-input");
+const addItemBtn = document.getElementById("add-item-btn");
+const itemUl = document.getElementById("items");
+const itemSpan = document.getElementById("item-count");
 
 // TODO 2: Write a function updateCount() that sets item-count's textContent
 // to the number of <li> elements currently in the list (items.children.length).
 
-
+function updateCount() {
+  itemSpan.textContent = itemUl.children.length;
+}
 // TODO 3: Add a "click" listener on #add-item-btn. Inside it:
 //   - read and trim the input's value; if empty, do nothing
 //   - create a new <li>, set its textContent to the value
@@ -17,3 +22,17 @@
 //   - add the <li> to the TOP of the list using items.prepend(li)
 //   - clear the input
 //   - call updateCount()
+
+addItemBtn.addEventListener("click", () => {
+  const trimName = itemInput.value.trim();
+  if (trimName === "") return;
+  const li = document.createElement("li");
+  li.textContent = trimName;
+  li.addEventListener("click", () => {
+    li.remove();
+    updateCount();
+  });
+  itemUl.prepend(li);
+  itemInput.value = "";
+  updateCount();
+});
